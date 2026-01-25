3c10ea5cfbc039fd452f7ec1c755e33eb2c941c6
Sebastián Rojas Vargas <rojxsvargxs2607@gmail.com>
2026-01-25 14:19:24 -0600

    Commit final de semana 2 Estructura

---
+++ Vehiculo.API/API/Controllers/VehiculoController.cs
@@ -0,0 +1,47 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoController : ControllerBase, IVehiculoController
+
+    {
+        private  IVehiculoFlujo _vehiculoFlujo;
+        private ILogger<VehiculoController> _logger;
+
+        public VehiculoController(IVehiculoFlujo vehiculoFlujo, ILogger<VehiculoController> logger)
+        {
+            _vehiculoFlujo = vehiculoFlujo;
+            _logger = logger;
+        }
+
+        public Task<IActionResult> Agregar(VehiculoRequest vehiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Editar(Guid Id, VehiculoRequest vehiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Eliminar(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
